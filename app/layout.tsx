f9997bce162c76eed6078090b3fee734a7afc714
Matheus Alves <mtalves27@gmail.com>
2024-02-27 18:00:54 -0300

    chore: remove layout shift in dialog opening

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("container min-h-screen", inter.className)}>
+      <body className={cn("min-h-screen", inter.className)}>
         {children}
       </body>
     </html>
